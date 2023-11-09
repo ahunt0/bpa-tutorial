48f040f8bd361b08076f61a8a3c4f67fc9b98642
clownmoney <32284257+clownmoney@users.noreply.github.com>
2023-11-08 21:17:05 -0600

    Add forgot password modal to login form (WIP)

--- src/Components/Auth/LoginForm.jsx
+++ src/Components/Auth/LoginForm.jsx
@@ -3,8 +3,19 @@ import { Input, Button, Divider } from "@nextui-org/react";
 import AuthBase from "./AuthBase";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
+import { useState } from "react";
+import ForgotPassModal from "./ForgotPassModal";
 
-export default function LoginForm() {
+const LoginForm = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const onOpen = () => {
+    setIsModalOpen(true);
+  };
+
+  const onClose = () => {
+    setIsModalOpen(false);
+  };
   return (
     <AuthBase>
       <motion.div initial={{ opacity: 0, scale: 0.7 }} animate={{ opacity: 1, scale: 1 }} exit={{ opacity: 0 }} transition={{ ease: "easeOut", duration: 0.3 }}>
@@ -16,9 +27,11 @@ export default function LoginForm() {
           <div className="md:w-96">
             <Input type="email" label="Email" className="mb-4" />
             <Input type="password" label="Password" className="mb-4" />
-            <a href="#" className="text-primary-500 hover:text-primary-600 ease-in-out duration-400">
-              Forgot password?
-            </a>
+            <p className="text-sm text-primary-500 hover:text-primary-600 ease-in-out duration-400">
+              <a href="#" onClick={onOpen}>
+                Forgot password?
+              </a>
+            </p>
             <Button color="primary" variant="shadow" className="w-full mt-4 font-bold">
               Login
             </Button>
@@ -32,6 +45,8 @@ export default function LoginForm() {
           </div>
         </div>
       </motion.div>
+      <ForgotPassModal isOpen={isModalOpen} setIsOpen={setIsModalOpen} />
     </AuthBase>
   );
-}
+};
+export default LoginForm;
