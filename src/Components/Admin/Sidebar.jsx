6d1c1e08d1f4002f1f4fbd436f28ee46395773ba
clownmoney <32284257+clownmoney@users.noreply.github.com>
2023-11-09 22:16:58 -0600

    Minor edits

--- src/Components/Admin/Sidebar.jsx
+++ src/Components/Admin/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Sidebar() {
-	return <div className="dark h-screen w-1/3 bg-default-400"></div>;
+  return <div className="dark h-screen w-1/5 bg-default-50"></div>;
 }
